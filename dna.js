63439f4c2c4bf6be60bfa0551ddcb41f6693ebd5
shitzkhn <shitzkhn@gmail.com>
2020-06-06 23:29:23 +0530

    first commit

---
+++ dna.js
@@ -0,0 +1,34 @@
+function DNA(genes) {
+  if (genes) {
+    this.genes = genes;
+  } else {
+    this.genes = [];
+    for (var i = 0; i < lifespan; i++) {
+      this.genes[i] = p5.Vector.random2D();
+      this.genes[i] = setMag(maxforce);
+    }
+  }
+
+  this.crossover = function (partner) {
+    var newgen = [];
+
+    var idx = floor(random(this.genes.length));
+    for (var i = 0; i < this.genes.length; i++) {
+      if (i > mid) {
+        newgen[i] = partner.genes[i];
+      } else {
+        newgen[i] = this.genes[i];
+      }
+    }
+    return new DNA(newgen);
+  };
+
+  this.mutation = function () {
+    for (var i = 0; i < this.genes.length; i++) {
+      if (random(i) < mutationrate) {
+        this.genes[i] = p5.Vector.random2D();
+        this.genes[i] = setmag(maxforce);
+      }
+    }
+  };
+}
