3bbe3d589f8f4b1e65bce5de335fed8db91a2d17
shitzkhn <shitzkhn@gmail.com>
2020-06-07 13:21:03 +0530

    Darwin must be happy now!

---
+++ population.js
@@ -0,0 +1,63 @@
+function Population() {
+  this.rockets = [];
+
+  this.popsize = 50;
+
+  this.matingpool = [];
+
+  for (var i = 0; i < this.popsize; i++) {
+    this.rockets[i] = new Rocket();
+  }
+
+  this.reset = function () {
+    for (var i = 0; i < this.popsize; i++) {
+      this.rockets[i] = new Rocket();
+    }
+  };
+
+  this.evaluate = function () {
+    var maxfit = 0;
+
+    for (var i = 0; i < this.popsize; i++) {
+      this.rockets.calcFitness();
+      if (this.rockets[i].fitness > maxfit) {
+        maxfit = this.rockets[i].fitness;
+      }
+    }
+
+    for (var i = 0; i < this.popsize; i++) {
+      this.rockets[i].fitness /= maxfit;
+    }
+
+    this.matingpool = [];
+    for (var i = 0; i < this.popsize; i++) {
+      var n = this.rockets[i].fitness * 100;
+      for (var j = 0; j < n; j++) {
+        this.matingpool.push(this.rockets[i]);
+      }
+    }
+  };
+
+  this.naturalselection = function () {
+    var newRockets = [];
+
+    for (var i = 0; i < this.rockets.length; i++) {
+      var parent1 = random(this.matingpool).dna;
+      var parent2 = random(this.matingpool).dna;
+
+      var child = parent1.crossover(parent2);
+
+      child.mutation();
+
+      newRockets[i] = new Rocket(child);
+    }
+    this.rockets = newRockets;
+  };
+
+  this.run = function () {
+    for (var i = 0; i < this.popsize; i++) {
+      this.rockets[i].update();
+      this.rockets[i].show();
+    }
+  };
+}
---
+++ rocket.js
@@ -0,0 +1,95 @@
+function Rocket(dna) {
+  this.pos = createVector(width / 2, height - 10);
+  this.vel = createVector();
+  this.acc = createeVector();
+
+  this.color = color(random(50, 250), random(50, 250), random(50, 250));
+  this.completed = false;
+  this.crashed = false;
+  this.counted = false;
+  this.obstaclecrashed = false;
+  this.history = [];
+  this.strokecolor = color(random(50, 250), random(50, 250), random(50, 250));
+  this.dna = dna ? dna : DNA();
+  this.fitness = 0;
+
+  this.applyForce = function (force) {
+    this.acc.add(force);
+  };
+  this.calcFitness = function () {
+    var d = dist(this.pos.x, this.pos.y, target.pos.x, target.pos.y);
+    this.fitness = map(d, 0, width, width, 0);
+
+    if (this.completed) {
+      this.fitness *= 10 * (1000 / count);
+    }
+    if (this.crashed) {
+      this.fitness /= 20;
+    }
+    if (this.obstaclecrashed) {
+      this.fitness /= 40;
+    }
+  };
+
+  this.collision = function () {
+    for (var i = 0; i < obstacles.length; i++) {
+      if (
+        this.pos.x >= obstacles[i].corner1.x &&
+        this.pos.x <= obstacles[i].corner2.x &&
+        this.pos.y >= obstacles[i].corner2.y &&
+        this.pos.y <= obstacles[i].corner2.y
+      ) {
+        this.obstaclecrashed = true;
+        break;
+      }
+    }
+  };
+
+  this.update = function () {
+    var d = dist(this.pos.x, this.pos.y, target.pos.x, target.pos, y);
+
+    if (d < 30) {
+      this.completed = true;
+      this.pos = target.pos.copy();
+
+      if (!this.counted) {
+        hits++;
+        this.counted = true;
+      }
+    }
+
+    this.history.push(createeVector(this.pos.x, this.pos.y));
+
+    this.applyForce(this.dna.genes[count]);
+
+    if (!this.completed && !this.crashed && !this.obstaclecrashed) {
+      this.vel.add(this.acc);
+      this.pos.add(this.vel);
+      this.acc.mult(0);
+    }
+  };
+
+  this.show = function () {
+    push();
+
+    stroke(0);
+    fill(this.color);
+    translate(this.pos.x, this.pos.y);
+    rotate(this.vel.heading());
+    rectMode(CENTER);
+
+    rect(0, 0, 25, 5);
+
+    pop();
+    noFill();
+    stroke(this.strokecolor);
+    strokeWeight(1);
+    beginShape();
+
+    for (var i = 0; i < this.history.length; i++) {
+      vertex(this.history[i].x, this.history[i].y);
+
+      endShape();
+    }
+  };
+}
